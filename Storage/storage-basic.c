717daa3aa87e457799190bfafc3e9f7923f5bbda
Yash <154449658+yashveerdalal@users.noreply.github.com>
2024-08-02 08:48:07 +0530

    Add files via upload

---
+++ Storage/storage-basic.c
@@ -0,0 +1,19 @@
+// code
+// golbale/static
+// stack
+// heap 
+
+//attributes in storage class
+//scope
+//default initial value
+// lifetime 
+
+
+// types of storage class
+// automatic
+//external
+//static 
+// register 
+
+// auto storge is the default storage 
+
